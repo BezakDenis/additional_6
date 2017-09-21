8e237074de9f8db1d899b7c0daff8ec963e852fc
bloodofden <bloodofden@gmail.com>
2017-09-22 01:27:01 +0300

    Improved method performance

--- src/index.js
+++ src/index.js
@@ -1,48 +1,62 @@
 module.exports = function zeros(expression) {
   let items = expression.slice(0,-1).split('!*');
-  let result = {
-  	countTwo: 0,
-  	countFive: 0
-  };
   let index;
+  let attribute;
+  let result = {
+    countTwo: 0,
+    countFive: 0
+  };
 
   if(!expression) 
-  	return -1;
+    return -1;
 
   while(items.length){
-  	index = items.length - 1;
+    index = items.length - 1;
 
-  	switch(items[index][items[index].length - 1]){
-  		case '!':{
-  			localCount(+items[index].slice(0, -1), 2, result);
-  		}break;
+    switch(items[index][items[index].length - 1]){
+      case '!':{
+        attribute = +items[index].slice(0, -1);
 
-  		default:{
-  			localCount(+items[index], 1, result);
-  		}break;
-  	}
+        if(attribute%2){
+           while(attribute%5){
+              attribute -= 2;
+            }
 
-  	items.pop();
+            localCount(attribute, 10, result);
+        }
+        else{
+            localCount(attribute, 2, result); 
+        }
+      }break;
+
+      default:{
+        localCount(+items[index], 1, result);
+      }break;
+    }
+
+    items.pop();
   }
 
   return result.countTwo > result.countFive ? 
   result.countFive : result.countTwo;
 }
 
-function localCount(number, step, result){	
-	while(number > 1){
-		result.countTwo += countOf(2, number);
-		result.countFive += countOf(5, number);
-		number -= step;
-	}
+function localCount(number, step, result){  
+  while(number > 1){
+    if(!(number%2) || !(number%5)){
+      result.countTwo += countOf(2, number);
+      result.countFive += countOf(5, number);
+    }
+    number -= step;
+  }
 }
 
 function countOf(item, number){
-	let result = 0;
+  let result = 0;
+  while(!(number%item)){
+    number /= item;
+    result++;
+  }
 
-	while(!(number%item)){
-		result++;
-		number /= item;
-	}
-	return result;
+  return result;
 }
